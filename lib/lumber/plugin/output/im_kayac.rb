1a025fda681f25104ca68ce793e5ec08f107cb3c
mizoR <suzunatsu@yahoo.com>
2013-06-24 15:23:46 +0900

    add im.kayac output plugin

---
+++ lib/lumber/plugin/output/im_kayac.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+module Lumber
+  module Plugin
+    module Output
+      class ImKayac
+        def initialize(params={})
+          @base_url   = "http://im.kayac.com/api/post/"
+          @username   = params[:username] or raise ArgumentError
+          @password   = params[:password]   || nil
+          @secret_key = params[:secret_key] || nil
+        end
+
+        def exec(entries)
+          uri = URI.parse "#{@base_url}#{@username}"
+
+          Net::HTTP.new(uri.host, uri.port).start do |http|
+            entries.each do |entry|
+              sig = @secret_key ? \
+                Digest::SHA1.new.update("#{entry.title}#{@secret_key}").to_s : nil
+
+              request = Net::HTTP::Post.new(uri.path)
+              request.set_form_data(
+                :handler  => entry.url,
+                :password => @password,
+                :sig      => sig,
+                :message  => entry.title,
+              )
+              http.request(request)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
