0dd3e9b88c9df3a69e9c4c05604ae5c06114b905
mizoR <suzunatsu@yahoo.com>
2013-06-26 19:04:56 +0900

    add pipe_spec

---
+++ spec/macaroni/core/pipe_spec.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+describe Macaroni::Core::Pipe do
+  it '#plug' do
+    plugin_class = double(:plugin_class)
+    plugin_class.should_receive(:new).exactly(3).times
+
+    pipe = Macaroni::Core::Pipe.new
+    pipe.send(:plug, plugin_class) {}
+    pipe.send(:plug, plugin_class) {}
+    pipe.send(:plug, plugin_class) {}
+    expect(pipe.instance_eval {@plugins.length}).to be 3
+  end
+
+  it '#exec' do
+    plugin = double(:plugin)
+    plugin.should_receive(:exec).and_return('TEST')
+
+    plugin_class = double(:plugin_class).stub(:new)
+    plugin_class.should_receive(:new).and_return(plugin)
+
+    pipe = Macaroni::Core::Pipe.new
+    pipe.send(:plug, plugin_class) {}
+    expect(pipe.exec).to eq 'TEST'
+  end
+end
+
