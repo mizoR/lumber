f4e869730c81d8ff478174dbb1eb678135dcc988
mizoR <suzunatsu@yahoo.com>
2013-06-27 13:18:05 +0900

    move DSL method definition

--- lib/macaroni/core/dsl.rb
+++ lib/macaroni/core/dsl.rb
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 require 'macaroni/core/pipe'
 
-def self.pipe(name, &block)
-  pipe = Macaroni::Core::Pipe.new
-  pipe.instance_eval(&block)
-  Macaroni::application.set_pipe(name, pipe)
+module Macaroni
+  module Core
+    module DSL
+      def pipe(name, &block)
+        pipe = Macaroni::Core::Pipe.new
+        pipe.instance_eval(&block)
+        Macaroni::application.set_pipe(name, pipe)
+      end
+    end
+  end
+end
+
+class << self
+  include Macaroni::Core::DSL
 end
 
